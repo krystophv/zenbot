f28519fdbac4b31731195c686f80011adafa2fa6
Chris McGee <krystophv@gmail.com>
2018-01-14 00:30:43 -0500

    little more work on the lstm neural strat

--- extensions/strategies/neural_lstm/strategy.js
+++ extensions/strategies/neural_lstm/strategy.js
@@ -11,35 +11,33 @@ module.exports = function container (get, set, clear) {
     name: 'neural_lstm',
     description: 'Use neural long short term memory learning to predict future price. Buy = mean(last 3 real prices) < mean(current & last prediction)',
     getOptions: function () {
-      this.option('period', 'period length - make sure to lower your poll trades time to lower than this value. Same as --periodLength', String, '1m')
-      this.option('periodLength', 'period length - make sure to lower your poll trades time to lower than this value. Same as --period', String, '1m')
-      this.option('activation_1_type', 'Neuron Activation Type: sigmoid, tanh, relu', String, 'sigmoid')
-      this.option('neurons_1', 'Neurons in layer 1 Shoot for atleast 100', Number, 1)
-      this.option('depth', 'Rows of data to predict ahead for matches/learning', Number, 5)
-      this.option('selector', 'Selector', String, 'Gdax.BTC-USD')
-      this.option('min_periods', 'Periods to calculate learn from', Number, 300)
-      this.option('min_predict', 'Periods to predict next number from', Number, 100)
-      this.option('momentum', 'momentum of prediction', Number, 0.9)
+      this.option('period', 'period length - make sure to lower your poll trades time to lower than this value. Same as --period_length', String, '1m')
+      this.option('period_length', 'period length - make sure to lower your poll trades time to lower than this value. Same as --period', String, '1m')
+      this.option('min_periods', 'Periods to calculate learn from', Number, 500)
+      this.option('momentum', 'momentum of prediction', Number, 0.5)
       this.option('decay', 'decay of prediction, use teeny tiny increments', Number, 0)
-      this.option('threads', 'Number of processing threads you\'d like to run (best for sim)', Number, 1)
       this.option('learns', 'Number of times to \'learn\' the neural network with past data', Number, 10)
       this.option('markup_pct', 'Defaulting a markup percent', Number, 0.05)
       this.option('memory_cells', 'Number of memory cells for LSTM network', Number, 6)
+      this.option('input_remember', 'Number of previous inputs the network remembers', Number, 3)
+      this.option('output_remember', 'Number of preivous outputs the network remembers', Number, 3)
+      this.option('cross_validate_pct', 'Percentage of samples to use for cross validation instead of training', Number, 0.3)
     },
     calculate: function (s) {
     },
     onPeriod: function (s, cb) {
       if (s.lstm_network === undefined) {
-        s.lstm_network = new neataptic.architect.Perceptron(5, 10,10,10, 2)
+        s.lstm_network = new neataptic.architect.NARX(
+          4, // input size
+          [ s.options.memory_cells, s.options.memory_cells ], // hiddenLayers
+          2, // output size
+          s.options.input_remember, // previous number of inputs to remember
+          s.options.output_remember  // previous number of outputs to remember
+        )
       }
-      if(
-        s.lookback.length > s.options.min_periods && 
-        s.lookback.length > s.options.min_predict) {
-        console.log('training')
-        let training_data = []
-        for (let i = 0; i < s.options.min_periods; i++) { 
-          training_data.push(s.lookback[i]) 
-        }
+      if(s.lookback.length > s.options.min_periods) {
+        //console.log('training')
+        let training_data = s.lookback.slice(0, s.options.min_periods) 
         training_data = training_data.reverse()
         const multi_dim_array = training_data.map((value)=>{
           return [ 
@@ -47,32 +45,34 @@ module.exports = function container (get, set, clear) {
             value.low,
             value.open,
             value.close,
-            value.volume 
+            // value.volume 
           ]
         })
         // max is highest high
         const max = math.max(multi_dim_array, 0)
-        const max_price = max[0]
-        const max_vol = max[4]
+        const max_price = 20000 //max[0] 
+        //const max_vol = max[4]
         // min is lowest low
         const min = math.min(multi_dim_array, 0)
-        const min_price = min[1]
-        const min_vol = min[4]
+        const min_price = 10000 //min[1]
+        //const min_vol = min[4]
         const divisor = (max_price-min_price)
+        const scalePrice = (value) => { return (value - min_price) / divisor }
+        //const scaleVolume = (vol) => { return (vol - min_vol) / (max_vol - min_vol) } 
         training_data = training_data.map((value, i, data) => {
           // normalize data for set between 0 and 1
           const input = [ 
-            (value.high-min_price)/divisor,
-            (value.low-min_price)/divisor,
-            (value.open-min_price)/divisor,
-            (value.close-min_price)/divisor,
-            (value.volume-min_vol)/(max_vol-min_vol) 
+            scalePrice(value.high),
+            scalePrice(value.low),
+            scalePrice(value.open),
+            scalePrice(value.close),
+            //scaleVolume(value.volume) 
           ]
           return { 
             input: input, 
             output: [ 
-              data[i+1] && (data[i+1].close-min_price)/divisor, 
-              data[i+2] && (data[i+2].close-min_price)/divisor 
+              data[i+1] && scalePrice(data[i+1].close), 
+              data[i+2] && scalePrice(data[i+2].close) 
             ]
           } 
         })
@@ -84,109 +84,56 @@ module.exports = function container (get, set, clear) {
 
         s.lstm_network.train(training_data, {
           log: 500,
-          iterations: 100,
-          clear: true,
-          error: 0.03,
-          //momentum: s.options.momentum
-          cost: neataptic.methods.cost.MAE
-          //dropout: 0.5
+          iterations: 1000,
+          //clear: true,
+          error: 0.01,
+          momentum: s.options.momentum,
+          rate: 0.1,
+          crossValidate: {
+            testSize: s.options.cross_validate_pct,
+            testError: 0.01
+          }
+          //cost: neataptic.methods.cost.MAE
+          //dropout: 0.5,
           //rate: 0.05,
         })
 
-        /*
-        for(var i in training_data){
-          var input = training_data[i].input
-          var output = s.lstm_network.activate([input])
-          if(parseInt(i) == training_data.length-1){
-            console.log(input)
-            console.log('input: ',
-              (input * scale).toFixed(2),
-              ' real ',
-              (training_data[i].output[0] * scale).toFixed(2),
-              ' output: ',
-              (output * scale).toFixed(2)
-            )
-          }
-        }
-        */
-
-        //for(var j = 0; j < 10; j++){
-        //var input = output
-        //var output = s.lstm_network.activate([input])
-        //console.log('next predict: ', output*scale)
-        //}
-        //if(Math.round(s.lstm_network.activate([s.period.close/scale])[0])){
         const network_output = s.lstm_network.activate([
-          (s.period.high-min_price)/divisor,
-          (s.period.low-min_price)/divisor,
-          (s.period.open-min_price)/divisor,
-          (s.period.close-min_price)/divisor,
-          (s.period.volume-min_vol)/(max_vol-min_vol)
+          scalePrice(s.period.high),
+          scalePrice(s.period.low),
+          scalePrice(s.period.open),
+          scalePrice(s.period.close),
+          // scaleVolume(s.period.volume)
         ])
+
         const predict_1 = network_output[0]
         const predict_2 = network_output[1]
-        console.log(`
+
+        const predict_1_rescaled = predict_1 * divisor + min_price
+        const predict_2_rescaled = predict_2 * divisor + min_price
+
+        /*console.log(`
          current: ${s.period.close},
-         prediction_1: ${predict_1*divisor+min_price}, 
-         prediction_2: ${predict_2*divisor+min_price}`)
-        if(predict_2 > (s.period.close-min_price)/divisor && predict_1 > (s.period.close-min_price)/divisor){
+         prediction_1: ${predict_1_rescaled}, 
+         prediction_2: ${predict_2_rescaled}`)
+        */
+        s.period.predict_1 = predict_1_rescaled
+        s.period.predict_2 = predict_2_rescaled
+
+        if(predict_2 > scalePrice(s.period.close) && predict_1 > scalePrice(s.period.close)){
           s.signal = 'buy'
         } else {
           s.signal = 'sell'
         }
       }
-      
-      // do the network thing
-      /*
-      var tlp = []
-      var tll = []
-      if (s.lookback[s.options.min_periods]) {
-        for (let i = 0; i < s.options.min_periods; i++) { tll.push(s.lookback[i].close) }
-        for (let i = 0; i < s.options.min_predict; i++) { tlp.push(s.lookback[i].close) }
-        var my_data = tll.reverse()
-        var learn = function () {
-          //Learns
-          for (var j = 0; j < s.options.learns; j++) {
-            for (var i = 0; i < my_data.length - s.neural.neuralDepth; i++) {
-              var data = my_data.slice(i, i + s.neural.neuralDepth)
-              var real_value = [my_data[i + s.neural.neuralDepth]]
-              var x = new convnetjs.Vol(data)
-              s.neural.trainer.train(x, real_value)
-              var predicted_values = s.neural.net.forward(x)
-            }
-          }
-        }
-        var predict = function(data) {
-          var x = new convnetjs.Vol(data)
-          var predicted_value = s.neural.net.forward(x)
-          return predicted_value.w[0]
-        }
-        learn()
-        var item = tlp.reverse()
-        s.prediction = predict(item)
-        s.mean = math.mean(s.lookback[0].close, s.lookback[1].close, s.lookback[2].close)
-        s.meanp = math.mean(s.prediction, oldmean)
-        oldmean = s.prediction
-      }
-      // NORMAL onPeriod STUFF here
-      global.meanp = s.meanp
-      global.mean = s.mean
-      //something strange is going on here
-      global.sig0 = global.meanp < global.mean
-      if ( global.sig0 === false ){
-        s.signal = 'sell'
-      }
-      else {
-        s.signal = 'buy'
-      } */
       cb()
       
     },
     onReport: function (s) {
       var cols = []
-      cols.push(z(8, n(global.mean).format('00000.000'), ' ')[global.meanp > global.mean ? 'green' : 'red'])
+      cols.push(z(8, n(s.period.predict_1).format('00000.000'), ' ')[s.period.predict_1 > s.period.close ? 'green' : 'red'])
       cols.push('    ')
-      cols.push(z(8, n(global.meanp).format('00000.000'), ' ')[global.meanp > global.mean ? 'green' : 'red'])
+      cols.push(z(8, n(s.period.predict_2).format('00000.000'), ' ')[s.period.predict_2 > s.period.close ? 'green' : 'red'])
       return cols
     },
   }
